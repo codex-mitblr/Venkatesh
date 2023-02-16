15b46f53f8cc97db611e8a4b05fb47fdc96ad5e8
Abhigyan Trips <abhigyantrips@gmail.com>
2023-02-16 21:26:04 +0530

    chore: update constants

--- venkatesh/constants.py
+++ venkatesh/constants.py
@@ -21,6 +21,18 @@ if TEST_GUILDS := os.getenv("TEST_GUILDS"):
 
 class Channels(NamedTuple):
     log = int(os.getenv("CHANNEL_LOG", 1035618172402925659))
+    memberlog = int(os.getenv("CHANNEL_MEMBERLOG", 1035619429658132532))
+
+
+class Departments(NamedTuple):
+    cxsd = 1035615903762612334
+    cxos = 1035615907503943711
+    cxcp = 1035615911664697480
+    cxad = 1035615915506683974
+    cxit = 1035615919768080404
+    cxds = 1035615923605880942
+    cxnat = 1035615930627129345
+    cxgd = 1035615934074847232
 
 
 class Roles(NamedTuple):
